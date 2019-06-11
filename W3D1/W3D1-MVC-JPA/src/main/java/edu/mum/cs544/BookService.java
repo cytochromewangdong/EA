40aaf9e5f19d20e28ae450f8089bd2d896f3b464
dongwang <wangdongliyanxiang@hotmail.com>
2019-06-10 22:01:14 -0500

    Finished homework

---
+++ W3D1/W3D1-MVC-JPA/src/main/java/edu/mum/cs544/BookService.java
@@ -0,0 +1,36 @@
+package edu.mum.cs544;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+public class BookService {
+	@Resource
+	private IBookDao bookDao;
+
+	public List<Book> getAll() {
+		return bookDao.findAll();
+	}
+
+	public void add(Book book) {
+		bookDao.save(book);
+	}
+
+	public Book get(int id) {
+//		return bookDao.findById(id).get();
+		return bookDao.getOne(id);
+	}
+
+	public void update(Book book) {
+		bookDao.save(book);
+	}
+
+	public void delete(int id) {
+		bookDao.deleteById(id);
+	}
+}
